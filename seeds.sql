6bd767b0997e7951348be3a0373fe6bfd40350ea
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-10-01 16:24:39 -0500

    working tables

--- seeds.sql
+++ seeds.sql
@@ -8,17 +8,17 @@ VALUES ("Sales"),
 ("Reception");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Rep", 8500000, 1),
-("Head of Sales", 9000000, 1),
-("Junior Sales Rep", 7000000, 1),
-("Customer Service Rep", 8000000, 2),
-("Head of Customer Service", 85000000, 2),
-("Accountant", 9500000, 3),
-("Head of Accounting", 10000000, 3),
-("Qualilty Assurance Rep", 8000000, 4),
-("Supplier Relations Rep", 8000000, 5),
-("Manager", 10000000, 6),
-("Receptionist", 6500000, 7);
+VALUES ("Sales Rep", 85000, 1),
+("Head of Sales", 90000, 1),
+("Junior Sales Rep", 70000, 1),
+("Customer Service Rep", 80000, 2),
+("Head of Customer Service", 850000, 2),
+("Accountant", 95000, 3),
+("Head of Accounting", 100000, 3),
+("Qualilty Assurance Rep", 80000, 4),
+("Supplier Relations Rep", 80000, 5),
+("Manager", 100000, 6),
+("Receptionist", 65000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Pam", "Halpert", 1, 1),
