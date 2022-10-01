c12399c9015ab589363ed2c168a18d07963b2f92
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-10-01 14:46:04 -0500

    populated schema.sql

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role_db (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL 
+);
+
+CREATE TABLE employee_db (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT NOT NULL,
+    manager-id INT
+);
